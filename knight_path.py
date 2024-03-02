fcd434c24a8ea9c243aa24a0e104a3406e99be09
William Wingate <wtwingate@gmail.com>
2024-03-02 17:38:56 -0500

    Start working on getting and translating user input

--- knight_path.py
+++ knight_path.py
@@ -1,3 +1,5 @@
+import re
+
 # A little program that calculates the shortest path for a Knight to
 # move from one square to another on a chessboard.
 
@@ -30,6 +32,39 @@ class Knight:
         return coords
 
 
+class Translator:
+    def __init__(self):
+        self.__translator_keys = {
+            "1": 0,
+            "2": 1,
+            "3": 2,
+            "4": 3,
+            "5": 4,
+            "6": 5,
+            "7": 6,
+            "8": 7,
+            "a": 0,
+            "b": 1,
+            "c": 2,
+            "d": 3,
+            "e": 4,
+            "f": 5,
+            "g": 6,
+            "h": 7,
+        }
+
+    def standard_to_numeric(self, start_pos, end_pos):
+        trans_start = []
+        trans_end = []
+
+        trans_start.append(self.__translator_keys[start_pos[0]])
+        trans_start.append(self.__translator_keys[start_pos[1]])
+        trans_end.append(self.__translator_keys[end_pos[0]])
+        trans_end.append(self.__translator_keys[end_pos[1]])
+
+        return trans_start, trans_end
+
+
 def find_path(start_pos, end_pos):
     queue = [Knight(start_pos)]
 
@@ -56,6 +91,29 @@ def print_path(path):
     print("Here is your path:")
     for pos in path:
         print(pos)
+
+
+def get_start_pos():
+    start_pos = None
+    while not valid_input(start_pos):
+        start_pos = input("Please enter your starting coordinates: ")
+    return start_pos
+
+
+def get_end_pos():
+    end_pos = None
+    while not valid_input(end_pos):
+        end_pos = input("Please enter your starting coordinates: ")
+    return end_pos
+
+
+def valid_input(input):
+    if len(input) != 2:
+        return False
+    elif re.match("^[a-h][1-8]$", input):
+        return True
+    else:
+        return False
 
 
 def main():
