9e1ceae7db68b48df7c8be30c6703247316bab5f
William Wingate <wtwingate@gmail.com>
2024-03-16 23:01:59 -0400

    Implement the insertion sort algorithm in Python

---
+++ insertion_sort.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+def main():
+    my_list = []
+    for i in range(20):
+        my_list.append(randint(0, 99))
+    print("Unsorted: ", my_list)
+    insertion_sort(my_list)
+    print("Sorted:   ", my_list)
+
+
+def insertion_sort(my_list):
+    """Sort list using the insertion sort algorithm"""
+    for i in range(len(my_list)):
+        j = i
+        while j > 0 and my_list[j] < my_list[j - 1]:
+            my_list[j], my_list[j - 1] = my_list[j - 1], my_list[j]
+            j -= 1
+    return my_list
+
+
+main()
