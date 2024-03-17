301fc2dc068128ba874f1508575597bd81a3249c
William Wingate <wtwingate@gmail.com>
2024-03-16 20:53:15 -0400

    Implement bubble sort algorithm in Python

---
+++ bubble_sort.py
@@ -0,0 +1,28 @@
+from random import randint
+
+
+def main():
+    my_list = []
+    for i in range(20):
+        my_list.append(randint(0, 99))
+    print(my_list)
+    bubble_sort(my_list)
+    print(my_list)
+
+
+def bubble_sort(my_list):
+    """Sort list using the bubble sort algorithm"""
+    end = len(my_list)
+    sorting = True
+    while sorting:
+        sorting = False
+        for i in range(1, end):
+            if my_list[i - 1] > my_list[i]:
+                sorting = True
+                tmp = my_list[i - 1]
+                my_list[i - 1] = my_list[i]
+                my_list[i] = tmp
+    return my_list
+
+
+main()
