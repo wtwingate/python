3ea391ba88d88714fde38ca76e1991f2b3d1118c
William Wingate <wtwingate@gmail.com>
2024-03-16 23:22:31 -0400

    Test how quickly the algorithm can sort 10,000 items
    This is of limited usefulness since it does not say anything about
    the Big O complexity of each algorithm, but it does help exemplify
    their relative performance.

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,21 @@
 from random import randint
+import time
 
 
 def main():
-    my_list = []
+    short_list = []
+    long_list = []
     for i in range(20):
-        my_list.append(randint(0, 99))
-    print("Unsorted: ", my_list)
-    bubble_sort(my_list)
-    print("Sorted:   ", my_list)
+        short_list.append(randint(0, 99))
+    print("Unsorted: ", short_list)
+    bubble_sort(short_list)
+    print("Sorted:   ", short_list)
+    for i in range(10000):
+        long_list.append(randint(0, 9999))
+    print("Sorting list with 10,000 elements:")
+    start = time.time()
+    bubble_sort(long_list)
+    print(f"Sorted in {time.time() - start} seconds")
 
 
 def bubble_sort(my_list):
--- merge_sort.py
+++ merge_sort.py
@@ -1,12 +1,20 @@
 from random import randint
+import time
 
 
 def main():
-    my_list = []
+    short_list = []
+    long_list = []
     for i in range(20):
-        my_list.append(randint(0, 99))
-    print("Unsorted: ", my_list)
-    print("Sorted:   ", merge_sort(my_list))
+        short_list.append(randint(0, 99))
+    print("Unsorted: ", short_list)
+    print("Sorted:   ", merge_sort(short_list))
+    for i in range(10000):
+        long_list.append(randint(0, 9999))
+    print("Sorting list with 10,000 elements:")
+    start = time.time()
+    merge_sort(long_list)
+    print(f"Sorted in {time.time() - start} seconds")
 
 
 def merge_sort(my_list):
