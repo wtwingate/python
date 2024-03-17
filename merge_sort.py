8cac531fb09faca1b19e61f140c63268d189dfd7
William Wingate <wtwingate@gmail.com>
2024-03-16 22:34:13 -0400

    Implement merge sort algorithm in Python

---
+++ merge_sort.py
@@ -0,0 +1,37 @@
+from random import randint
+
+
+def main():
+    my_list = []
+    for i in range(20):
+        my_list.append(randint(0, 99))
+    print(my_list)
+    print(merge_sort(my_list))
+
+
+def merge_sort(my_list):
+    """Sort list using the merge sort algorithm"""
+    if len(my_list) < 2:
+        return my_list
+    left = merge_sort(my_list[: len(my_list) // 2])
+    right = merge_sort(my_list[len(my_list) // 2 :])
+    return merge(left, right)
+
+
+def merge(left, right):
+    """Merge items from two lists from least to greatest"""
+    merged = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged
+
+
+main()
